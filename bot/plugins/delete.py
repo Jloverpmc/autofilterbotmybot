3006494701538d1d414f60f69b63fe851bc49a5d
Jloverpmc <134128724+Jloverpmc@users.noreply.github.com>
2025-08-26 17:46:41 +0530

    Update delete.py

--- bot/plugins/delete.py
+++ bot/plugins/delete.py
@@ -1,13 +1,18 @@
 from bot.database.store import get_settings
 from bot.utils.helpers import schedule_auto_delete, format_autodelete_time
-async def send_file_with_autodelete(bot, chat_id:int, file_id:str, caption:str):
+
+async def send_file_with_autodelete(bot, chat_id: int, file_id: str, caption: str):
     s = await get_settings()
-    secs = s.get('autodelete_seconds') or 1800
-    note = s.get('autodelete_note') or '⚠️ This file will be auto-deleted in {autodelete_time}.'
-    expired = s.get('autodelete_expired') or '❌ The file has been deleted after {autodelete_time}.'
-    human = format_autodelete_time(secs)
-    final_caption = (caption + '\n\n' if caption else '') + note.format(autodelete_time=human)
-    m = await bot.send_document(chat_id, file_id, caption=final_caption)
-    # schedule delete
-    await schedule_auto_delete(bot, m.chat.id, m.id, secs, expired.format(autodelete_time=human))
-    return m
+    secs = s.get("autodelete_seconds") or 0
+    note = s.get("autodelete_note") or None
+    expired = s.get("autodelete_expired") or None
+
+    if secs and secs > 0:
+        human = format_autodelete_time(secs)
+        send_caption = (caption + "\n\n" if caption else "") + (note.format(autodelete_time=human) if note else "")
+        m = await bot.send_document(chat_id, file_id, caption=send_caption)
+        await schedule_auto_delete(bot, m.chat.id, m.id, secs, expired.format(autodelete_time=human) if expired else None)
+        return m
+    else:
+        return await bot.send_document(chat_id, file_id, caption=caption)
+        
