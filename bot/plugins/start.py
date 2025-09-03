8992fdc93f8822627b17a985e547ea81ad0ebee4
Jloverpmc <134128724+Jloverpmc@users.noreply.github.com>
2025-09-03 22:51:20 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -5,7 +5,7 @@ from bot.database.store import get_settings
 @Client.on_message(filters.command("start") & filters.private)
 async def start(client, message):
     user_id = message.from_user.id
-    settings = get_settings(user_id)
+    settings = await get_settings()
 
     start_msg = settings.get("start_msg", "👋 Hello, I’m your AutoFilter Bot!")
     start_pic = settings.get("start_pic")
