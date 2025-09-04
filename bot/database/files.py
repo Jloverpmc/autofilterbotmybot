affeacd867db560ca3459a5b0ffbdc39d9811566
Jloverpmc <134128724+Jloverpmc@users.noreply.github.com>
2025-09-04 17:01:01 +0530

    Update files.py

--- bot/database/files.py
+++ bot/database/files.py
@@ -1,23 +1,19 @@
-# bot/database/files.py
+from motor.motor_asyncio import AsyncIOMotorClient
 import bot.config as config
-try:
-    from motor.motor_asyncio import AsyncIOMotorClient
-    _db_client = AsyncIOMotorClient(config.MONGO_URL) if config.MONGO_URL else None
-    _db = _db_client[config.DB_NAME] if _db_client else None
-except Exception:
-    _db = None
 
-_mem_count = 0
+if not config.MONGO_URL:
+    raise ValueError("MONGO_URL is not set!")
+
+_db_client = AsyncIOMotorClient(config.MONGO_URL)
+_db = _db_client[config.DB_NAME]  # e.g., "autofilter"
+
+async def init_meta():
+    """Ensure meta collection has the files counter initialized"""
+    await _db.meta.update_one({"_id": "files"}, {"$setOnInsert": {"count": 0}}, upsert=True)
 
 async def inc_files(n: int = 1):
-    global _mem_count
-    if _db:
-        await _db.meta.update_one({"_id": "files"}, {"$inc": {"count": n}}, upsert=True)
-    else:
-        _mem_count += n
+    await _db.meta.update_one({"_id": "files"}, {"$inc": {"count": n}}, upsert=True)
 
 async def total_files() -> int:
-    if _db:
-        doc = await _db.meta.find_one({"_id": "files"}) or {}
-        return int(doc.get("count", 0))
-    return _mem_count
+    doc = await _db.meta.find_one({"_id": "files"}) or {}
+    return int(doc.get("count", 0))
